31ef107edbda714417671a70c505339f93806f75
Irina0rlova <irine.palamarchuk@gmail.com>
2025-12-30 11:17:47 +0200

    Initial Commit

---
+++ MiniBookPlayer/MiniBookPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBookPlayerApp.swift
+//  MiniBookPlayer
+//
+//  Created by Iryna Orlova on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MiniBookPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
